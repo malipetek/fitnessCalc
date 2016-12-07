89b3e85517d157ccf12b2584444b8e54d32e47cc
malipetek <malipetek@gmail.com>
2016-12-07 18:26:35 +0300

    last

--- assets/js/consumption.js
+++ assets/js/consumption.js
@@ -1,176 +1,214 @@
 var ConsumptionForm = (function(container, options) {
 
-    var
-        $container = $(container),
-        $form = $($container.find("form")[0]),
-        $measures = $("#consumption_measure"),
-        $ndbno = $("#consumption_ndbno"),
-        $what = $("#consumption_what"),
-        $when = $("#consumption_when"),
-        $quantity = $("#consumption_quantity");
+  var
+    $container = $(container),
+    $form = $($container.find("form")[0]),
+    $measures = $("#consumption_measure"),
+    $ndbno = $("#consumption_ndbno"),
+    $what = $("#consumption_what"),
+    $when = $("#consumption_when"),
+    $quantity = $("#consumption_quantity");
 
-    var recipe_id;
-    var selection_type;
+  var recipe_id;
+  var selection_type;
 
-    var that = this;
+  var that = this;
 
-    function reset() {
-        $form[0].reset();
-        $when.data("DateTimePicker").date(new Date())
+  function reset() {
+    $form[0].reset();
+    $when.data("DateTimePicker").date(new Date())
 
-    }
+  }
 
-    function showLoader() {
-        materialadmin.AppCard.addCardLoader($container);
-    }
+  function showLoader() {
+    materialadmin.AppCard.addCardLoader($container);
+  }
 
-    function hideLoader() {
-        materialadmin.AppCard.removeCardLoader($container);
-    }
+  function hideLoader() {
+    materialadmin.AppCard.removeCardLoader($container);
+  }
 
-    function init() {
-        showLoader();
+  function init() {
+    showLoader();
 
-        $when.datetimepicker({
-            defaultDate: new Date(),
-            icons:{
-                time: 'fa fa-clock-o',
-                date: 'fa fa-calendar',
-                up: 'fa fa-chevron-up',
-                down: 'fa fa-chevron-down',
-                previous: 'fa fa-chevron-left',
-                next: 'fa fa-chevron-right',
-                today: 'fa fa-screenshot',
-                clear: 'fa fa-trash',
-                close: 'fa fa-remove'
-            }
-        });
+    $when.datetimepicker({
+      defaultDate: new Date(),
+      icons: {
+        time: 'fa fa-clock-o',
+        date: 'fa fa-calendar',
+        up: 'fa fa-chevron-up',
+        down: 'fa fa-chevron-down',
+        previous: 'fa fa-chevron-left',
+        next: 'fa fa-chevron-right',
+        today: 'fa fa-screenshot',
+        clear: 'fa fa-trash',
+        close: 'fa fa-remove'
+      }
+    });
 
-        reset();
+    reset();
 
-        $form.submit(function(event){
-            event.preventDefault();
-            submit();
-        });
+    $form.submit(function(event) {
+      event.preventDefault();
+      submit();
+    });
 
-        $what.autocomplete({
-            lookup: function(query, done) {
-                $.ajax({
-                    url: '/api/food/',
-                    type: 'GET',
-                    data: {
-                        q: query,
-                    },
-                    success: function(res) {
-                        var suggestions = $.map(res, function(item){
-                            return {value: item.name, data: item}
-                        });
-                        var result = {"suggestions": suggestions};
-                        done(result);
-                    }
-                });
-            },
-            onSelect: function (suggestion) {
-                if (suggestion.data.type && suggestion.data.type == "recipe") {
-                    recipe_id = suggestion.data.id;
-                    selection_type = "recipe"
+    $what.autocomplete({
+      lookup: function(query, done) {
+        $.ajax({
+          url: 'http://api.nal.usda.gov/ndb/search/?format=json&sort=n&max=25&offset=0&api_key=omcaFN9P4v5xb3l2VM7EqPyxwWRPjkg31EivJ4Jb',
+          type: 'GET',
+          data: {
+            q: query
+          },
+          success: function(res) {
+            if (res.errors) {
+              $('.search-result-panel').slideUp();
+            } else {
+              $('.search-result-panel').slideDown();
+              $list = $('.search-result-list');
+              $list.find('li').slideUp('fast', function() {
+                $(this).remove();
+              });
+              var listArray = res.list.item;
+              $.each(listArray, function(index, item) {
+                var li_element = document.createElement(
+                  'li');
+                li_element.classList.add('list-group-item');
+                li_element.setAttribute('data-food-db-no',
+                  item.ndbno);
+                var splittedArray = item.name.split(',');
+                var firstPart = splittedArray[0];
+                var itemNameTrimmed = "";
+                var brand;
+
+                if (splittedArray.length == 2) {
+                  itemNameTrimmed = firstPart;
                 } else {
-                    selection_type = "food"
+                  $.each(splittedArray, function(index,
+                    item) {
+                    if (item.indexOf('UPC') !== -1 ||
+                      item.indexOf('GTIN') !== -1) {
+                      console.log(item);
+                    } else if (index == 0) {
+                      brand = "(" + item + ")";
+                    } else {
+                      itemNameTrimmed += item + ", ";
+                    }
+                  });
+                  itemNameTrimmed += brand;
                 }
-
-                $ndbno.val(suggestion.data.ndbno)
-                update_measures(suggestion.data)
+                li_element.innerHTML = itemNameTrimmed;
+                li_element.style.display = 'none';
+                $list.append(li_element);
+                $(li_element).slideDown();
+              });
             }
+          }
         });
-        hideLoader()
-    };
+      }
+    });
+    delaylay = $what.autocomplete('option', 'delay');
+    console.log('delay: ' + $what.autocomplete('option', 'delay'));
+    hideLoader()
+  };
 
-    function update_measures(data) {
-        showLoader();
 
-        function fill_measure_selection(items) {
-            $measures.html("");
-            $.map(items, function(item){
-                $measures.append("<option>" + item + "</option>");
-            });
-        }
+  $(document).on('click', '.search-result-list > li', function() {
+    $val = $(this).text();
+    $dbid = $(this).attr('data-food-db-no');
+    console.log("value: " + $val + " id: " + $dbid);
+  });
 
-        if (!data.ndbno && data.type == "recipe") {
-            fill_measure_selection(["portion"])
-            hideLoader();
-            return;
-        }
 
-        $.ajax({
-            url: '/api/food/' + $ndbno.val() + '/measures/',
-            type: 'GET',
-            complete: hideLoader,
-            error: function() {
-                toastr.error("units could not be fetched :(")
-            },
-            success: function(res) {
-                fill_measure_selection(res)
-            }
-        });
+
+  function update_measures(data) {
+    showLoader();
+
+    function fill_measure_selection(items) {
+      $measures.html("");
+      $.map(items, function(item) {
+        $measures.append("<option>" + item + "</option>");
+      });
     }
 
-    function submit() {
-        showLoader();
-        submit_consumption(
-            $ndbno.val(),
-            $what.val(),
-            moment($when.data("DateTimePicker").date()).tz("UTC").format(),
-            $quantity.val(),
-            $measures.val(),
-            {
-                "onError": function(e) {
-                    hideLoader();
-                    toastr.error("consumption couldn't be added. " + e)
-                },
-                "onSuccess": function (entry) {
-                    hideLoader();
-                    reset();
-                    toastr.success("consumption has been added successfully. ", "success");
-                    if (options["onConsumptionSaved"])
-                        options["onConsumptionSaved"](entry)
-                }
-            }
-        );
+    if (!data.ndbno && data.type == "recipe") {
+      fill_measure_selection(["portion"])
+      hideLoader();
+      return;
     }
 
+    $.ajax({
+      url: '/api/food/' + $ndbno.val() + '/measures/',
+      type: 'GET',
+      complete: hideLoader,
+      error: function() {
+        toastr.error("units could not be fetched :(")
+      },
+      success: function(res) {
+        fill_measure_selection(res)
+      }
+    });
+  }
 
-    function submit_consumption(ndbno, what, when, quantity, measure, callback) {
-        var postData =  {
-            "selection_type": selection_type,
-            "what": what,
-            "when": when,
-            "measure": measure,
-            "quantity": quantity
+  function submit() {
+    showLoader();
+    submit_consumption(
+      $ndbno.val(),
+      $what.val(),
+      moment($when.data("DateTimePicker").date()).tz("UTC").format(),
+      $quantity.val(),
+      $measures.val(), {
+        "onError": function(e) {
+          hideLoader();
+          toastr.error("consumption couldn't be added. " + e)
+        },
+        "onSuccess": function(entry) {
+          hideLoader();
+          reset();
+          toastr.success("consumption has been added successfully. ",
+            "success");
+          if (options["onConsumptionSaved"])
+            options["onConsumptionSaved"](entry)
         }
+      }
+    );
+  }
 
-        if (selection_type == "recipe") {
-            postData.id = recipe_id
-            postData.category = "rc"
-        } else {
-            postData.ndbno = ndbno
-            postData.category = "fc"
-        }
 
-        $.ajax({
-            url: '/api/entries/',
-            type: 'POST',
-            data: postData,
-            error: function(e) {
-                if (callback.onError)
-                    callback.onError(e);
-            },
-            success: function(res) {
-                callback.onSuccess(res);
-            }
-        });
+  function submit_consumption(ndbno, what, when, quantity, measure,
+    callback) {
+    var postData = {
+      "selection_type": selection_type,
+      "what": what,
+      "when": when,
+      "measure": measure,
+      "quantity": quantity
     }
 
-    init();
+    if (selection_type == "recipe") {
+      postData.id = recipe_id
+      postData.category = "rc"
+    } else {
+      postData.ndbno = ndbno
+      postData.category = "fc"
+    }
+
+    $.ajax({
+      url: '/api/entries/',
+      type: 'POST',
+      data: postData,
+      error: function(e) {
+        if (callback.onError)
+          callback.onError(e);
+      },
+      success: function(res) {
+        callback.onSuccess(res);
+      }
+    });
+  }
+
+  init();
 
 });
 
@@ -310,10 +348,3 @@ var ConsumptionForm = (function(container, options) {
 
 
 // }
-
-
-
-
-
-
-
